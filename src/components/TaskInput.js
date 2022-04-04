d71be7b26172339f233fcfde9cf7f715971cfbc3
Carlos Diaz <cdiaz@nycfllc.com>
2022-04-04 17:40:47 -0400

    Added the background image and color to the page. Also, created the main container, the header and the input.

---
+++ src/components/TaskInput.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css';
+function TaskInput() {
+  return (
+    <div className='new-todo'>
+        <div className='check'>
+            <div className='check-mark'>
+               
+            </div>
+        </div>
+        <div className='new-todo-input'>
+            <form>
+                <input type="text" placeholder="Create a new todo..."></input>
+            </form>
+        </div> 
+    </div>
+  )
+}
+
+export default TaskInput
